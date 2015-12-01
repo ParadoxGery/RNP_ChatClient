8a4e6584b0b5f1f44da983e2d456805cdb392c0e
Gery <gerhard.wagner@haw-hamburg.de>
2015-12-01 10:51:27 +0100

    udp init

---
+++ src/de/haw/rnp/chatclient/connection/UDPConnection.java
@@ -0,0 +1,35 @@
+package de.haw.rnp.chatclient.connection;
+
+public class UDPConnection implements Connection {
+	
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void send(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String receive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public boolean connect(String srv, int port) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
